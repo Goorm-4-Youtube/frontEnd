d1a491ccb9df17bad710389131aadacefcce4fe8
whiter18 <whiter018@gmail.com>
2022-08-08 13:04:06 +0900

    change history
    show history when user is authenticated

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {VideoDto} from "../video-dto";
 import {VideoService} from "../video.service";
 import {UserService} from "../user.service";
+import {OidcSecurityService} from "angular-auth-oidc-client";
 
 @Component({
   selector: 'app-history',
@@ -12,13 +13,17 @@ export class HistoryComponent implements OnInit {
 
   viewedVideos: Array<VideoDto> = [];
 
-  constructor(private videoService: VideoService, private userService: UserService) { }
-
+  isAuthenticated: boolean = false;
+  constructor(private oidcSecurityService: OidcSecurityService,private videoService: VideoService, private userService: UserService) { }
   ngOnInit(): void {
-    this.userService.getHistory().subscribe(response => {
-      this.viewedVideos = response;
+    this.oidcSecurityService.isAuthenticated$.subscribe(({isAuthenticated}) =>{
+      this.isAuthenticated = isAuthenticated;
+      if (this.isAuthenticated){
+        this.userService.getHistory().subscribe(response => {
+          this.viewedVideos = response;
+        })
+      }
     })
-
   }
 
 }
