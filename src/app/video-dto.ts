9a06a7c86a7bd8d0734dfb69c678148c6788a8ac
whiter18 <whiter018@gmail.com>
2022-08-14 20:00:05 +0900

    sort
    sort

--- src/app/video-dto.ts
+++ src/app/video-dto.ts
@@ -2,14 +2,18 @@ export interface VideoDto{
 
   id: string;
   title: string;
+  videoStatus: string;
+  description: string;
+
   userId: string;
   userName: string;
-  description: string;
-  tags: Array<string>;
+
   videoUrl: string;
-  videoStatus: string;
   thumbnailUrl: string;
+
   likeCount: number;
   dislikeCount: number;
   viewCount: number;
+
+  tags: Array<string>;
 }
